bb02ed65e0c6a74ed81b703ce7e7cfc8c5138b40
Damian <c.damiankucharski@gmail.com>
2018-06-19 13:10:52 +0200

    Initial Commit

---
+++ cmake-build-debug/CMakeFiles/DiffProject.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DiffProject.dir/main.cpp.o"
+  "DiffProject.pdb"
+  "DiffProject"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DiffProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/DiffProject.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for DiffProject.
+# This may be replaced when dependencies are built.
