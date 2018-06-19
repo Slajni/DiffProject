3e6307c2098e376c861e10ec5410c02efd161e39
Damian <c.damiankucharski@gmail.com>
2018-06-19 13:58:51 +0200

    add parameter checker function

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <fstream>
+#include <cstring>
 
 using namespace std;
 
@@ -12,10 +13,25 @@ struct Difference
     string secondFileVersion;
 };
 
+int parameterChecker(int argc, char ** argv) // check if parameters were passed correctly
+{
+    if(argc != 3)
+        return 0;
+    string str1(argv[1]);
+    string str2(argv[2]);
+    if(str1.length()>=5 && str1.length()>=5)
+    {
+        str1 = str1.substr(str1.length() - 4, 4);
+        str2 = str2.substr(str2.length() - 4, 4);
 
+        if(str1 == ".txt" && str2 == ".txt")
+            return 1;
+        return 0;
+    }
+}
 
 int main(int argc, char ** argv)
 {
-    std::cout << "Hello, World!" << std::endl;
+    cout << parameterChecker(argc, argv);
     return 0;
 }
