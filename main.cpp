2423995fed4f6edc5bd7dff8b6efec8539014342
Damian <c.damiankucharski@gmail.com>
2018-06-19 13:22:52 +0200

    created difference struct

--- main.cpp
+++ main.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
 #include <vector>
+#include <fstream>
 
 using namespace std;
 
-
+struct Difference
+{
+    int startPos;
+    int endPos;
+    string firstFileVersion;
+    string secondFileVersion;
+};
 
 int main()
 {
