74bfa7ba029cee077a05f7b704e347019d73fcb4
Damian <c.damiankucharski@gmail.com>
2018-06-20 09:45:51 +0200

    updated findDifferences function for the same strings

--- main.cpp
+++ main.cpp
@@ -5,14 +5,18 @@
 
 using namespace std;
 
+class Line;
+class Difference;
+
+vector <Line> differentLines;
+
 class Difference
 {
+public:
     int startPos;
     int endPos;
     string firstFileVersion;
     string secondFileVersion;
-
-public:
 
     Difference(int startPos, int endPos, const string & firstFileVersion, const string & secondFileVersion)
     {
@@ -50,7 +54,7 @@ public:
             string differenceString1 = "";
             string differenceString2 = "";
 
-            if(line1.length() == line2.length())
+            if(line1.length() == line2.length()) // if lines are of the same size
             {
                 int startPos = -1;
                 int endPos = -1;
@@ -58,16 +62,42 @@ public:
 
                 for(int i = 0; i < line1.length(); i++)
                 {
-                    if(line1[i]!=line2[i] && startPos == -1)
+                    if(line1[i]!=line2[i] && startPos == -1) // if it's new difference
                     {
                         startPos = i;
                         differenceString1 += line1[i];
                         differenceString2 += line2[i];
                     }
+                    else if(line1[i]!=line2[i] && startPos != -1) // still the same difference, so only updating difference strings
+                    {
+                        differenceString1 += line1[i];
+                        differenceString2 += line2[i];
+                    }
+                    else if (line1[i] == line2[i] && startPos != -1) // end of the difference
+                    {
+                        endPos = i-1;
+                        thisLine->differences.emplace_back(startPos, endPos, differenceString1, differenceString2);
+                        startPos = -1;
+                        endPos = -1;
+                    }
                 }
+                if(endPos == -1 && startPos != -1) // if line ended and last difference is still not complete, then the end of the difference is the end of the line
+                {
+                    endPos = (int)line1.length()-1;
+                    thisLine->differences.emplace_back(startPos, endPos, differenceString1, differenceString2);
+                }
+                differentLines.push_back(*thisLine);
             }
         }
         }
+
+    void printDifferences()
+    {
+        for(auto const & value : differences)
+        {
+            cout << endl << "In positions " << value.startPos << " - " << value.endPos << endl << "Version 1: " << value.firstFileVersion << endl << "Version 2: " << value.secondFileVersion << endl;
+        }
+    }
 };
 
 
@@ -99,9 +129,9 @@ inline int openFiles(char ** argv, ifstream & File1, ifstream & File2)
 
 int main(int argc, char ** argv)
 {
-    cout << parameterChecker(argc, argv);
     ifstream firstFile, secondFile;
     openFiles(argv, firstFile, secondFile);
+
 
     return 0;
 }
